dc1a47deee1fe345ea88a3a7a6b4fc51efe115ac
TaeryungLee <trlee94@snu.ac.kr>
2025-10-05 22:02:05 +0900

    babel eval

--- TRAIN_evaluator_272_BABEL.sh
+++ TRAIN_evaluator_272_BABEL.sh
@@ -2,5 +2,5 @@ export HF_ENDPOINT=https://hf-mirror.com
 cd Evaluator_272
 # huggingface-cli download --resume-download distilbert/distilbert-base-uncased --local-dir ./deps/distilbert-base-uncased
 ln -s ../data/babel_272 ./datasets/babel_272
-python -m train --cfg configs/configs_evaluator_272/H3D-TMR_BABEL.yaml --cfg_assets configs/assets.yaml --batch_size 64 --nodebug
+python -m train --cfg configs/configs_evaluator_272/H3D-TMR_BABEL.yaml --cfg_assets configs/assets.yaml --batch_size 256 --nodebug
 cd ..
