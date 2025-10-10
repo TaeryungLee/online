ee526f933e0827896e6cfe473678e8f8281cee7d
TaeryungLee <trlee94@snu.ac.kr>
2025-10-10 22:30:59 +0900

    text enc

---
+++ get_text_enc.py
@@ -0,0 +1,35 @@
+import os
+
+
+# from sentence_transformers import SentenceTransformer
+# t5_model = SentenceTransformer('sentencet5-xxl/')
+# t5_model.eval()
+# for p in t5_model.parameters():
+#     p.requires_grad = False
+
+text_path_list = ['data/babel_272/texts', 'data/humanml3d_272/texts', 'data/babel_272_stream/train_stream_text', 'data/babel_272_stream/val_stream_text']
+
+
+
+
+for text_path in text_path_list:
+    out_path = text_path.replace('data', 'text_enc')
+    os.makedirs(out_path, exist_ok=True)
+
+    for file in os.listdir(text_path):
+        out_file = os.path.join(out_path, file.replace('.txt', '.npy'))
+        with open(os.path.join(text_path, file), 'r') as f:
+            text = f.readlines()
+            enc_list = []
+
+            for line in text:
+                line_split = line.strip().split('#')
+                caption = line_split[0]
+                text_enc = t5_model.encode(caption)
+                enc_list.append(text_enc)
+            
+            text_enc = np.array(enc_list)
+            np.save(out_file, text_enc)
+            breakpoint()
+
+
